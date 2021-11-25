41b94c9880c1af4e321487b17562170f3c83215f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-25 18:50:04 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/02_repeatString/repeatString.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = (str, nb) => {
+  let temp = str;
 
+  if (nb < 0) {
+    return "ERROR";
+  } else if (nb == 0) {
+    str = "";
+    return str;
+  } else {
+    for (let i = 1; i < nb; i++) {
+      temp += str;
+    }
+    return temp;
+  }
 };
 
 // Do not edit below this line
