6e9bdf2989ba4874bd8533fe8093949e39a3320f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-27 17:43:09 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
 const removeFromArray = (arr, ...args) => {
   args.forEach((arg) => {
     let argIndex = arr.indexOf(arg);
+    if (argIndex < 0) {
+      return;
+    }
     arr.splice(argIndex, 1);
   });
+  return arr;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
