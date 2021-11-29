fd85a894c0bc40af31bc68fe034c13eb1a076acf
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-29 10:43:28 +0100

    first commit

---
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/05_sumAll/sum-all.js
@@ -0,0 +1,23 @@
+const sumAll = (a, b) => {
+	let result = 0;
+	let temporary;
+
+	if (a < 0 || b < 0) {
+		return 'ERROR';
+	}
+
+	if (a > b) {
+		temporary = a;
+		a = b;
+		b = temporary;
+	}
+
+	for (let i = a; i <= b; i++) {
+		result += i;
+	}
+
+	return (result);
+};
+
+// Do not edit below this line
+module.exports = sumAll;
