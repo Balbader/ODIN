19c02b2dcf7a660e4bdd49ccbcfe1139a1fe9447
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-29 16:26:13 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/09_palindromes/palindromes.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = string_ => {
+	const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+	string_ = string_.toLowerCase().replace(regex, '');
+	string_ = string_.split(' ').join('');
+	let reversedString = '';
+	reversedString = string_.split('').reverse().join('');
 
+	if (string_ === reversedString) {
+		return true;
+	}
+
+	return false;
 };
 
 // Do not edit below this line
