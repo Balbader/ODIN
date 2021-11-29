936ed0c9707f4f5ecc9dca53d7ec963071f4e269
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-29 14:59:35 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/07_tempConversion/temp-conversion.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/07_tempConversion/temp-conversion.js
@@ -1,13 +1,24 @@
-const ftoc = function() {
+const round = (value, precision) => {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
-};
+const ftoc = fahr => {
+  let result;
+  fahr = ((fahr - 32) * 5 / 9);
+  result = round(fahr, 1);
+  return result;
+}
 
-const ctof = function() {
-
-};
+const ctof = cels => {
+  let result;
+  cels = ((cels * (9 / 5)) + 32);
+  result = round(cels, 1);
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
