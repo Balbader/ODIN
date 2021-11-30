72a4237823017066e407faa87138228b947afea8
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-30 12:43:26 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = (position) => {
+  const num = 100;
+  let fibonacci = [0, 1];
 
+  if (position < 0) {
+    return 'OOPS';
+  }
+
+  for (let i = 0; i < num; i++) {
+    fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+  }
+
+  return (parseInt((fibonacci[position])));
 };
 
 // Do not edit below this line
