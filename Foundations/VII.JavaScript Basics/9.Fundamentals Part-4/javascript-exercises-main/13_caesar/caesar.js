3cb1798f802fec4d7b65a809d48b4b5d759cb980
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-30 21:22:53 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/13_caesar/caesar.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/13_caesar/caesar.js
@@ -1,6 +1,10 @@
-const caesar = (str, shift) => {
+const caesar = (string_, shift) => {
 	const alphabetLength = 26;
-	return [...str].map((letter) => {
+	if (shift < 0) {
+		shift = 26 + (shift % 26);
+	}
+
+	return [...string_].map(letter => {
 		const isLetter = (/^[a-z]$/i).test(letter);
 
 		if (!(isLetter)) {
@@ -10,12 +14,9 @@ const caesar = (str, shift) => {
 		const isUpperCase = letter >= 'A' && letter <= 'Z';
 		const start = isUpperCase ? 65 : 97;
 		const newCodePoint = (((letter.codePointAt(0) - start) + shift) % alphabetLength) + start;
-		const newShift = (((letter.codePointAt(0) - shift) + start) % alphabetLength) + shift;
-		if (shift < 0) {
-			return String.fromCodePoint(newShift);
-		}
 		return String.fromCodePoint(newCodePoint);
 	}).join('');
 };
+
 // Do not edit below this line
 module.exports = caesar;
