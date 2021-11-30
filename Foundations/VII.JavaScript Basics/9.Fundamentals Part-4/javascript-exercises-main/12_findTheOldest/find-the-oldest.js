7b56c5ded2a3e03f0bc22a0eb76903ec7513056f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-30 17:23:57 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/12_findTheOldest/find-the-oldest.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/12_findTheOldest/find-the-oldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = people => {
+	let count = 0;
+	let result;
+	const thisYear = new Date().getFullYear();
+	for (const person of people) {
+		const age = (person.yearOfDeath - person.yearOfBirth)
+			|| (thisYear - person.yearOfBirth);
+		if (age > count) {
+			count = age;
+			result = person;
+		}
+	}
 
+	return result;
 };
 
 // Do not edit below this line
