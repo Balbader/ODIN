558591bac8f09e8919e2006a1bd6b78d9f3fb874
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-30 21:17:59 +0100

    update file

--- Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/13_caesar/caesar.js
+++ Foundations/VII.JavaScript Basics/9.Fundamentals Part-4/javascript-exercises-main/13_caesar/caesar.js
@@ -1,49 +1,21 @@
-// Const shift = (letter, k, code) => {
-// return String.fromCharCode(((letter.charCodeAt() - code + k) % 26) + code);
-// }
+const caesar = (str, shift) => {
+	const alphabetLength = 26;
+	return [...str].map((letter) => {
+		const isLetter = (/^[a-z]$/i).test(letter);
 
-// const caesar = (s, k) => {
-// return s.replace(/[a-z]/g, char => shift(char, k, 97)).replace(/[A-Z]/g, char => shift(char, k, 65));
-// }
-
-const caesar = (string, shift) => {
-	let c = 0;
-	let i = 0;
-
-	while (string[i]) {
-		if (string[i] >= 'A' && string[i] <= 'Z') {
-			c = string[i] = 'A';
-			c += shift;
-			while (c < 0) {
-				c += 26;
-			}
-
-			while (c > 25) {
-				c -= 26;
-			}
-
-			string[i] = c + 'A';
+		if (!(isLetter)) {
+			return letter;
 		}
 
-		if (string[i] >= 'a' && string[i] <= 'z') {
-			c = string[i] = 'a';
-			c += shift;
-			while (c < 0) {
-				c += 26;
-			}
-
-			while (c > 25) {
-				c -= 26;
-			}
-
-			string[i] = c + 'a';
+		const isUpperCase = letter >= 'A' && letter <= 'Z';
+		const start = isUpperCase ? 65 : 97;
+		const newCodePoint = (((letter.codePointAt(0) - start) + shift) % alphabetLength) + start;
+		const newShift = (((letter.codePointAt(0) - shift) + start) % alphabetLength) + shift;
+		if (shift < 0) {
+			return String.fromCodePoint(newShift);
 		}
-
-		i++;
-	}
-
-	return string;
+		return String.fromCodePoint(newCodePoint);
+	}).join('');
 };
-
 // Do not edit below this line
 module.exports = caesar;
