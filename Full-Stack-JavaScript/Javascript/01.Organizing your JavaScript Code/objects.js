34eec5a50f2d083b107b9240ea6d5562591f3faa
Balbader <15971200+Balbader@users.noreply.github.com>
2021-12-08 22:54:25 +0100

    update file

--- Full-Stack-JavaScript/Javascript/01.Organizing your JavaScript Code/objects.js
+++ Full-Stack-JavaScript/Javascript/01.Organizing your JavaScript Code/objects.js
@@ -3,36 +3,36 @@
 // ============================================================================
 
 // Ex 1
-const playerOneName = 'Tim';
-const playerTwoName = 'Jenn';
-const playerOneMaker = 'X';
-const playerTwoMaker = 'O';
+const playerOneName = "Tim";
+const playerTwoName = "Jenn";
+const playerOneMaker = "X";
+const playerTwoMaker = "O";
 
 // Ex 2
 const playerOne = {
-	name: 'Mark',
-	marker: 'XX',
+  name: "Mark",
+  marker: "XX",
 };
 
 const playerTwo = {
-	name: 'Sarah',
-	marker: 'YY',
+  name: "Sarah",
+  marker: "YY",
 };
 
-console.log(playerOne);
-console.log(playerTwo);
+//console.log(playerOne);
+//console.log(playerTwo);
 
 // Ex 3
-const printName = player => player.name;
+const printName = (player) => player.name;
 
-console.log(printName(playerOne));
+//console.log(printName(playerOne));
 
-const gameOver = winningPlayer => {
-	console.log('YaY');
-	console.log(`${winningPlayer.name} is the winner ^^`);
+const gameOver = (winningPlayer) => {
+  console.log("YaY");
+  console.log(`${winningPlayer.name} is the winner ^^`);
 };
 
-gameOver(playerTwo);
+//gameOver(playerTwo);
 
 // ============================================================================
 // Object Constructors
@@ -40,38 +40,48 @@ gameOver(playerTwo);
 
 // Ex 1
 function Player(name, marker) {
-	this.name = name;
-	this.marker = marker;
-	this.sayName = function () {
-		console.log(name);
-	};
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function () {
+    console.log(name);
+  };
 }
 
-const player1 = new Player('balou', '**');
-const player2 = new Player('mimi', '&&');
-player1.sayName();
-player2.sayName();
+const player1 = new Player("balou", "**");
+const player2 = new Player("mimi", "&&");
+//player1.sayName();
+//player2.sayName();
 
 // Exercise
 function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-	this.info = function () {
-		return `${title} by ${author}, ${pages}, ${read}`;
-	};
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  };
 }
 
 const theHobbit = new Book(
-	'The Hobbit',
-	'J.R.R. Tolkien',
-	'295 pages',
-	'not read yet',
+  "The Hobbit",
+  "J.R.R. Tolkien",
+  "295 pages",
+  "not read yet"
 );
 
-console.log(theHobbit.info());
+//console.log(theHobbit.info());
 
 // ============================================================================
 // The Prototype
 // ============================================================================
+function Student(name, grade) {
+  this.name = name;
+  this.grade = grade;
+}
+
+Student.prototype.sayName = function () {
+  console.log(this.name);
+};
+
+const studen1 = new Student("Mike", "A+");
